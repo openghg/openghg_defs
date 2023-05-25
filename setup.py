85d013843bd4ea3585a50faa6fcf153815f9347a
Gareth Jones <oxidising@gmail.com>
2023-05-25 14:47:46 +0100

    WIP: Conda packaging works

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/openghg/openghg_defs",
     packages=setuptools.find_packages(include=["openghg_defs"]),
-    package_data={"": ["data/*", "py.typed"]},
+    package_data={"openghg_defs": ["data/*", "data/domain/*", "py.typed"]},
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
