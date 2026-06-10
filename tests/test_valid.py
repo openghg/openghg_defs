34e1d38c646e4b83bb074bed783c0030aeca164a
61825162+CaderIdris@users.noreply.github.com <61825162+CaderIdris@users.noreply.github.com>
2026-06-10 11:07:19 +0100

    Preparing for modifications to use importlib
    Problem:
    
    - `uv` and `pytest` were core dependencies of the package, despite not
      being required for operation.
    - Tests were very simple, made them more comprehensive to ensure no
      regressions after changes were made.
    
    Solution:
    
    - Moved `pytest` to dev dependency group
    - Removed uv as a dependency,
      it is not required to install/work on packages using uv.
    
    Added:
    
    - `uv.lock` file, so virtual environment can now be set up with a simple
      `uv sync` command.

--- tests/test_valid.py
+++ tests/test_valid.py
@@ -1,19 +1,30 @@
+from pathlib import Path
 import json
+
 from openghg_defs import site_info_file, species_info_file, domain_info_file
 
 
 # NOTE - these tests should be expanded to check all
 # data matches a simple schema for site info etc
 def test_site_info_valid():
-    site_info = json.loads(site_info_file.read_text())
-    assert site_info
+    with site_info_file.open("rb") as interface:
+        pkg_json = json.load(interface)
+    with Path("./openghg_defs/data/site_info.json").open("rb") as local:
+        local_json = json.load(local)
+    assert pkg_json == local_json
 
 
 def test_species_info_valid():
-    species_info = json.loads(species_info_file.read_text())
-    assert species_info
+    with species_info_file.open("rb") as interface:
+        pkg_json = json.load(interface)
+    with Path("./openghg_defs/data/species_info.json").open("rb") as local:
+        local_json = json.load(local)
+    assert pkg_json == local_json
 
 
 def test_domain_info_valid():
-    domain_info = json.loads(domain_info_file.read_text())
-    assert domain_info
+    with domain_info_file.open("rb") as interface:
+        pkg_json = json.load(interface)
+    with Path("./openghg_defs/data/domain_info.json").open("rb") as local:
+        local_json = json.load(local)
+    assert pkg_json == local_json
