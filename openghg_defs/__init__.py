dc3e663528bac3afa5173218cf11bfa5fdb93146
61825162+CaderIdris@users.noreply.github.com <61825162+CaderIdris@users.noreply.github.com>
2026-06-10 14:39:48 +0100

    Removed duplication and added spaces
    Problem:
    
    - The examples are largely the same, repeated three times.
    - There was no spacing between the variables.
    
    Solution:
    
    - Moved one example to module level docstring and removed all examples
      from variable docstrings.
    - Added spaces between variables for readability.

--- openghg_defs/__init__.py
+++ openghg_defs/__init__.py
@@ -1,4 +1,14 @@
-"""Definitions for sites, domains and species used in OpenGHG."""
+"""Definitions for sites, domains and species used in OpenGHG.
+
+Examples
+--------
+>>> import json
+>>> from openghg_defs import openghg_defs_files
+>>> site_info_json = openghg_defs_files / "site_info.json"
+>>> with site_info_json.open("rb") as js:
+...     site_info = json.load(js)
+
+"""
 from importlib.resources import files
 import sys
 
@@ -13,54 +23,25 @@ openghg_defs_files: Traversable = files("openghg_defs.data")
 This is a `Traversable` (similar to `pathlib.Path`) object that can be used
 to access the other files in the package.
 
-Examples
---------
->>> import json
->>> from openghg_defs import openghg_defs_files
->>> site_info_json = openghg_defs_files / "site_info.json"
->>> with site_info_json.open("rb") as js:
-...     site_info = json.load(js)
-
 """
+
 site_info_file: Traversable = openghg_defs_files / "site_info.json"
 """Link to measurement site information relevant to OpenGHG.
 
 This is a `Traversable` (similar to `pathlib.Path`) object that can be used
 to access the file via a context manager.
-
-Examples
---------
->>> import json
->>> from openghg_defs import site_info_file
->>> with site_info_file.open("rb") as js:
-...     site_info = json.load(js)
-
 """
+
 species_info_file: Traversable = openghg_defs_files / "species_info.json"
 """Link to atmospheric species information relevant to OpenGHG.
 
 This is a `Traversable` (similar to `pathlib.Path`) object that can be used
 to access the file via a context manager.
-
-Examples
---------
->>> import json
->>> from openghg_defs import species_info_file
->>> with species_info_file.open("rb") as js:
-...     species_info = json.load(js)
-
 """
+
 domain_info_file: Traversable = openghg_defs_files / "domain_info.json"
 """Link to measurement domain information relevant to OpenGHG.
 
 This is a `Traversable` (similar to `pathlib.Path`) object that can be used
 to access the file via a context manager.
-
-Examples
---------
->>> import json
->>> from openghg_defs import domain_info_file
->>> with domain_info_file.open("rb") as js:
-...     domain_info = json.load(js)
-
 """
