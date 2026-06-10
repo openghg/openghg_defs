9995dada8447f0c5a467ea79e4d6d55a98ddf770
61825162+CaderIdris@users.noreply.github.com <61825162+CaderIdris@users.noreply.github.com>
2026-06-10 13:32:16 +0100

    Updated to use importlib
    Problem:
    
    - Code was not using `importlib` to surface its files, instead it was
      using a hacky solution via `pathlib`.
    
    Solution:
    
    - Code now uses `importlib` to return `Traversable` objects that can be
      opened with a context manager.
    	- The type hinting for `Traversable` objects changed between 3.10
    	  and 3.11 so a fix was introduced based on `sys.version_info`
    
    Added:
    
    - Extra tests for the `importlib` functionality.
    - Ruff for linting
    	- Far too many ruff rules
    - Ty for type checking
    - Docstrings to all four variables used to import data files, may be
      useful for context when importing using an IDE.
    - Tox to automate the testing of multiple versions of Python, as well
      as ruff and ty

--- openghg_defs/__init__.py
+++ openghg_defs/__init__.py
@@ -1,9 +1,66 @@
-from pathlib import Path
+"""Definitions for sites, domains and species used in OpenGHG."""
+from importlib.resources import files
+import sys
 
-module_root = Path(__file__).resolve().parent
+if sys.version_info < (3,11):
+    from importlib.abc import Traversable
+else:
+    from importlib.resources.abc import Traversable
 
-data_path: Path = module_root / "data"
+openghg_defs_files: Traversable = files("openghg_defs.data")
+"""Base directory for OpenGHG Definition files.
 
-site_info_file: Path = data_path / "site_info.json"
-species_info_file: Path = data_path / "species_info.json"
-domain_info_file: Path = data_path / "domain_info.json"
+This is a `Traversable` (similar to `pathlib.Path`) object that can be used
+to access the other files in the package.
+
+Examples
+--------
+>>> import json
+>>> from openghg_defs import openghg_defs_files
+>>> site_info_json = openghg_defs_files / "site_info.json"
+>>> with site_info_json.open("rb") as js:
+...     site_info = json.load(js)
+
+"""
+site_info_file: Traversable = openghg_defs_files / "site_info.json"
+"""Link to measurement site information relevant to OpenGHG.
+
+This is a `Traversable` (similar to `pathlib.Path`) object that can be used
+to access the file via a context manager.
+
+Examples
+--------
+>>> import json
+>>> from openghg_defs import site_info_file
+>>> with site_info_file.open("rb") as js:
+...     site_info = json.load(js)
+
+"""
+species_info_file: Traversable = openghg_defs_files / "species_info.json"
+"""Link to atmospheric species information relevant to OpenGHG.
+
+This is a `Traversable` (similar to `pathlib.Path`) object that can be used
+to access the file via a context manager.
+
+Examples
+--------
+>>> import json
+>>> from openghg_defs import species_info_file
+>>> with species_info_file.open("rb") as js:
+...     species_info = json.load(js)
+
+"""
+domain_info_file: Traversable = openghg_defs_files / "domain_info.json"
+"""Link to measurement domain information relevant to OpenGHG.
+
+This is a `Traversable` (similar to `pathlib.Path`) object that can be used
+to access the file via a context manager.
+
+Examples
+--------
+>>> import json
+>>> from openghg_defs import domain_info_file
+>>> with domain_info_file.open("rb") as js:
+...     domain_info = json.load(js)
+
+"""
